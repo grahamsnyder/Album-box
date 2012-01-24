f8078660d01bfeed26b36db941e993923ca62c36
grahamsnyder <grahamsnyder@postmaster.co.uk>
2012-01-24 22:13:52 +0000

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source :rubygems
+gem 	"scrobbler2", 	">=0.0.4", :require => "scrobbler2"
+gem	"activesupport",">=3.1.1", :require => "active_support/all"
+gem	"sinatra", 	">=1.3.2"
---
+++ albumboxgen.rb
@@ -0,0 +1,55 @@
+require 'sinatra'
+require 'active_support/all'
+require 'scrobbler2'
+GS_API_KEY = '0d01292c6827570c72a647903cc94e8b'
+Scrobbler2::Base.api_key = GS_API_KEY
+
+get '/' do
+	@title = 'Album Box'
+	erb :home
+end
+
+post '/' do
+  @username=params[:username]
+  @period= params[:period]
+  @albums = get_data(@username,@period)
+  erb :albumbox
+end
+
+get '/:cgi' do
+  #params = /widget?username=(?<username>[^&]*)&period=(?<period>.*)/.match(:cgi)
+  #params = /widget?period=(?<period>[^&]*)&username=(?<username>.*)/.match(:cgi) if params.nil?
+  #return nil if params.nil?
+  @username=params[:username]
+  @period= params[:period]
+  @albums = get_data(@username,@period)
+  erb :albumbox
+end
+
+def get_data(username,period)
+  user = Scrobbler2::User.new(username)
+    
+  #puts "=" * (username.length + 16)
+  begin
+  top_albums = user.top_albums(nil,{:period => period})["album"][1..6]
+  rescue NoMethodError
+  return erb :failed
+  end
+
+  albums = {
+  :artist_names => [],
+  :artist_urls  => [],
+  :album_names  => [],
+  :album_urls   => [],
+  :image_urls   => []
+  }
+
+  top_albums.each do |a| 
+	  albums[:artist_names] << a["artist"]["name"]
+	  albums[:artist_urls]  << a["artist"]["url"]
+	  albums[:album_names]  << a["name"]
+	  albums[:album_urls]   << a["url"]
+	  albums[:image_urls]   << a["image"][3]["#text"]
+	end
+  albums
+end
